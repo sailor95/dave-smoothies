eb6e59b1d4cb3d736a55b1aa2da1cfad589fe0e5
David Huang <bugman195@gmail.com>
2020-10-15 22:58:12 +0800

    Add multi ingredient func

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -4,13 +4,23 @@
     <form @submit.prevent="AddSmoothie">
       <div class="field title">
         <label for="title">Smoothie Title:</label>
-        <input type="text" name="title" v-model="title">
+        <input type="text" name="title" v-model="title" />
+      </div>
+      <div v-for="(ing, idx) in ingredients" :key="idx">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" v-model="ingredients[idx]">
       </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add an ingredient:</label>
-        <input type="text" name="add-ingredient">
+        <input
+          type="text"
+          name="add-ingredient"
+          @keydown.tab.prevent="addIng"
+          v-model="another"
+        />
       </div>
       <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn pink">Add Smoothie</button>
       </div>
     </form>
@@ -22,19 +32,30 @@ export default {
   name: 'AddSmoothie',
   data() {
     return {
-      title: null
-    }
+      title: null,
+      another: null,
+      ingredients: [],
+      feedback: null,
+    };
   },
   methods: {
     AddSmoothie() {
-      console.log(this.title)
-    }
-  }
-}
+      console.log(this.title);
+    },
+    addIng() {
+      if (this.another) {
+        this.ingredients.push(this.another);
+        this.another = null;
+      } else {
+        this.feedback = 'Must add a value';
+      }
+    },
+  },
+};
 </script>
 
 <style>
-.add-smoothie{
+.add-smoothie {
   margin-top: 60px;
   padding: 20px;
   max-width: 500px;
