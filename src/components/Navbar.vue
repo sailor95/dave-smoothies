ca0f797ab9f3f73df4cd174bd1bc6e70acb4b0f0
buugman195 <bugman195@gmail.com>
2020-11-05 23:11:47 +0800

    Rename Nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,11 @@
   <div class="navbar">
     <nav class="nav-extended indigo darken-2">
       <div class="nav-content">
-        <router-link :to="{name: 'Index'}">
-          <span class="nav-title">Ninja Smoothies</span>
+        <router-link :to="{ name: 'Index' }">
+          <span class="nav-title">Dave Smoothies</span>
         </router-link>
         <a href="" class="btn-floating btn-large halfway-fab pink">
-          <router-link :to="{name: 'AddSmoothie'}">
+          <router-link :to="{ name: 'AddSmoothie' }">
             <i class="material-icons">add</i>
           </router-link>
         </a>
@@ -19,9 +19,9 @@
 export default {
   name: 'Navbar',
   data() {
-    return {}
-  }
-}
+    return {};
+  },
+};
 </script>
 
 <style>
